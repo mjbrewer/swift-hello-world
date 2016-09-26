528b61ee511e0f28f23301880e5a8f686b8ee996
mjbrewer <mjbrewer@us.ibm.com>
2016-09-26 13:56:13 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyHello",
     dependencies: [
-        .Package(url: "https://github.com/mjbrewer/swift-hello-world.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/mjbrewer/Tree-Loop1.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/mjbrewer/swift-hello-world.git", majorVersion: 0, minor: 12),
     ]
 )
